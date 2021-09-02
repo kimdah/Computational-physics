09d45668c41038a83cf6af35aff63bd58069296c
cecilmkl <cklarpaas@gmail.com>
2021-09-02 15:31:29 +0200

    Tester commit

---
+++ helloworld.cpp
@@ -0,0 +1,12 @@
+// First C++ program
+
+#include <iostream> // "pasting" standard C++ functionality so we can use std::cout
+
+int main()
+{
+  // Print message to screen
+  std :: cout << "Hello World!\n";
+
+  // Return 0 when done
+  return 0;
+}
