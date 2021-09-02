b298bb9eef1b5f5f2f734aae3302453f6108739e
cecilmkl <cklarpaas@gmail.com>
2021-09-02 10:57:09 +0200

    test

---
+++ helloworld.cpp
@@ -0,0 +1,18 @@
+// cpp = sourcefile
+
+#include <iostream>
+/*
+Include = paste content of file iostream at the top of the program
+          Similar tp pythons import
+<> around file when file is in standard library
+*/
+
+
+int main()
+{
+  // Print a message to screen
+  std::cout << "Hello, World!\n";
+
+  // Return 0 when done
+  return 0;
+}
