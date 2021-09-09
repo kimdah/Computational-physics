68521607e29fb25ec8bbe370d94c2b86d8e86598
cecilmkl <cklarpaas@gmail.com>
2021-09-09 11:43:02 +0200

    Fixing general algorithm

--- prosjekt1/main.cpp
+++ prosjekt1/main.cpp
@@ -6,6 +6,7 @@
 #include <iomanip>
 using namespace std;
 
+double f(double x);
 arma::vec general_algorithm(arma::vec a, arma::vec b, arma::vec c, arma::vec g, int npoints);
 
 int main(int argc, const char * argv[]) {
@@ -17,6 +18,9 @@ int main(int argc, const char * argv[]) {
     double x_min = 0.0;
     double x_max = 1.0;
     double h = (x_max - x_min) / npoints;
+    // Boundary conditions:
+    double u_min = 0; // u(0) = 0
+    double u_max = 0;  // u(1)=0
 
     int width = 12;
     int prec = 4;
@@ -50,26 +54,28 @@ int main(int argc, const char * argv[]) {
 
     arma::vec v = arma::vec(n); // Declaring empty solution vector
 
-    arma::vec g = arma::vec(n); //???
-    g(0) = h*h*100*exp(-10*(0+h)) + 0; // h^2 * f_1 for special case.
+    // Defining the g vector:
+    arma::vec g = arma::vec(n);
+    g(0) = h*h*100*exp(-10*x(0)) + u_min; // h^2*f1 + u(0)
+    g(n-1) = h*h*100*exp(-10*x(n-1)) + u_max; // h^2*f1 + u(1)
+
+    for (int i = 1; i <= n-2; i++){
+      g(i) = h*h*100*exp(-10*x(i))
+    }
 
     arma::vec v = general_algorithm(a,b,c,g,n);
 
     //opening file
     ofstream ofile;
-    ofile.open ("data.txt");
+    ofile.open ("approx_general.txt");
 
     //setting up the x-array and the solutions to the function u, and printing it to file
     for (i=0 ; i <= npoints-1 ; i++){
         ofile << setw(width) << setprecision(prec) << scientific << x(i)
               << setw(width) << setprecision(prec) << scientific << v(i) << endl;
-
     }
     //close file
     ofile.close();
-
-
-
 
     return 0;
 }
@@ -86,17 +92,21 @@ arma::vec general_algorithm(arma::vec a, arma::vec b, arma::vec c, arma::vec g, 
     arma::vec v = arma::vec(n); // solution vector
     double tmp; // variable to reduce FLOPs
 
-    for (int i = 0; i <= npoints -1; i++){ // n elements (n-1)?
+    for (int i = 1; i <= npoints -1; i++){ // n elements (n-1)?
       tmp = a(i) / b(i-1) * c(i-1);
       btilde(i) = b(i) - tmp;
       gtilde(i) = g(i) - tmp;
     }
 
-    v(n-1) = gtilde(n) / btilde(n); // Last element can now be found directly
+    v(n-1) = gtilde(n-1) / btilde(n-1); // Last element can now be found directly
 
-    for (int j = n-2; j >= 1; j--){ // n-2 elements
+    for (int j = n-2; j >= 0; j--){ // n-1 elements
       v(j) = (gtilde(j) - (c(j) * v(j+1))) / btilde(j);
     }
     return v;
 
+}
+
+double f(double x){
+  return 100*exp(-10*x);
 }
