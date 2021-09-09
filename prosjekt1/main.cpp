db0721cf5593ddf78178b4d88cdeb3f483ec77e2
cecilmkl <cklarpaas@gmail.com>
2021-09-09 10:46:51 +0200

    Changed vectors to arma::vec

--- prosjekt1/main.cpp
+++ prosjekt1/main.cpp
@@ -12,8 +12,8 @@ int main(int argc, const char * argv[]) {
 
     int i;
     int npoints = 10;
-    double u[npoints];
-    double x[npoints];
+    arma::vec u = arma::vec(npoints);
+    arma::vec x = arma::vec(npoints);
     double x_min = 0.0;
     double x_max = 1.0;
     double h = (x_max - x_min) / npoints;
@@ -28,18 +28,18 @@ int main(int argc, const char * argv[]) {
     //setting up the x-array and the solutions to the function u, and printing it to file
     for (i=0 ; i <= npoints ; i++){
 
-        x[i] = h*i;
-        u[i] = 1 - (1 - exp(-10)) * x[i] - exp(-10 * x[i]);
-        ofile << setw(width) << setprecision(prec) << scientific << x[i] << setw(width) << setprecision(prec) << scientific << u[i] << endl;
+        x(i) = h*i;
+        u(i) = 1 - (1 - exp(-10)) * x(i) - exp(-10 * x(i));
+        ofile << setw(width) << setprecision(prec) << scientific << x(i)
+              << setw(width) << setprecision(prec) << scientific << u(i) << endl;
 
     }
     //close file
     ofile.close();
 
 
-    // problem 7
-    //double v[npoints];
-
+    // Problem 7
+    // special case:
     arma::vec a = arma::vec(n).fill(-1.);
     arma::vec b = arma::vec(n).fill(2.);
     arma::vec c = arma::vec(n).fill(-1);
@@ -48,13 +48,25 @@ int main(int argc, const char * argv[]) {
     a(0) = 0.;
     c(npoints) = 0.;
 
-    arma::vec v = arma::vec(n);
+    arma::vec v = arma::vec(n); // Declaring empty solution vector
 
     arma::vec g = arma::vec(n); //???
     g(0) = h*h*100*exp(-10*(0+h)) + 0; // h^2 * f_1 for special case.
 
-
     arma::vec v = general_algorithm(a,b,c,g,n);
+
+    //opening file
+    ofstream ofile;
+    ofile.open ("data.txt");
+
+    //setting up the x-array and the solutions to the function u, and printing it to file
+    for (i=0 ; i <= npoints-1 ; i++){
+        ofile << setw(width) << setprecision(prec) << scientific << x(i)
+              << setw(width) << setprecision(prec) << scientific << v(i) << endl;
+
+    }
+    //close file
+    ofile.close();
 
 
 
@@ -74,7 +86,7 @@ arma::vec general_algorithm(arma::vec a, arma::vec b, arma::vec c, arma::vec g, 
     arma::vec v = arma::vec(n); // solution vector
     double tmp; // variable to reduce FLOPs
 
-    for (int i = 0; i <= npoints -1; i++){ // n elements
+    for (int i = 0; i <= npoints -1; i++){ // n elements (n-1)?
       tmp = a(i) / b(i-1) * c(i-1);
       btilde(i) = b(i) - tmp;
       gtilde(i) = g(i) - tmp;
