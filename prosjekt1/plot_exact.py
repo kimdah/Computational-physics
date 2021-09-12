bc0644e972b1f0d6f49937edbbfc97d2717e1176
cecilmkl <cklarpaas@gmail.com>
2021-09-13 00:05:22 +0200

    Small changes to plotfiles

--- prosjekt1/plot_exact.py
+++ prosjekt1/plot_exact.py
@@ -1,9 +1,8 @@
-
-#from script import *
 import matplotlib.pyplot as plt
 import numpy as np
-data = np.loadtxt('exact_data.txt')
+data = np.loadtxt('exact_data1000.txt')
+plt.title('Exact solution of Poisson eq.\nHere with N=1000')
 plt.xlabel('x')
-plt.ylabel('u(x)')
-plt.plot(data[:,0], data [:,1], 'ro')
+plt.ylabel('u(x)= 1- (1-exp(-10)x - exp(-10*x))')
+plt.plot(data[:,0], data [:,1])
 plt.savefig('problem2.pdf')
