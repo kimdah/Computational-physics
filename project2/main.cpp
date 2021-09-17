39d7ab5c0fd1a46da832f4d756dda979050a7b51
fridtjrg <fridtjof@gjengset.com>
2021-09-17 11:15:48 +0200

    Task 4a)

--- project2/main.cpp
+++ project2/main.cpp
@@ -1,4 +1,6 @@
+#include <armadillo>
 
+double find_max_value();
 
 
 int main(int argc, char const *argv[]) {
@@ -14,3 +16,24 @@ int main(int argc, char const *argv[]) {
 
   return 0;
 }
+
+
+double find_max_value(arma::mat A, int& k, int& l){
+
+double max_value = 0;
+N = arma::size(A)(0); //i is dimension N of matrix A
+
+
+for (int i=0; i<=N+1; j++){
+        for (int j=0; j<=N+1; j++){
+          
+          if(abs(A(i,j))>abs(max_value)){
+            max_value= A(i,j)
+            k = i, l=j;
+          }
+    }
+    }
+
+return max_value
+}
+
