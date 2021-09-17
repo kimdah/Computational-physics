991282f5e1ce456d9a30955703ac23d9ed5bae34
cecilmkl <cklarpaas@gmail.com>
2021-09-17 11:20:36 +0200

    Matrix constructed

--- project2/main.cpp
+++ project2/main.cpp
@@ -1,16 +1,33 @@
+#include <iostream>
+#include <armadillo>
 
+using namespace std;
 
 
 int main(int argc, char const *argv[]) {
 
   // Problem 3
   int N = 6;    // size of matrix
+  int n = N+1; // number of steps
+  double h = 1./n; // stepsize
   arma::mat A = arma::mat(N, N).fill(0.);
 
-  for (int i = 0; i < N; i++){
-    
+  cout << "h = " << h << endl;
+
+  for (int i = 0; i < N; i++){  // row
+    for (int j = 0; j < N; j++){ // // column
+      if (i == j){
+        A(i,j) = 2./(h*h);
+      } else if ((j == i+1) || (i == j+1)){
+        A(i,j) = -1./ (h*h);
+      }
+    }
   }
-  double A_ij = A(i,j); //Assign element (i,j) of the matrix A to A_ij.
+  cout << A;
+
+
+
+
 
   return 0;
 }
