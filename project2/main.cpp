48684bb4888bea6127fdb22dd33b7c3c660fb61e
cecilmkl <cklarpaas@gmail.com>
2021-09-17 10:46:45 +0200

    Main

---
+++ project2/main.cpp
@@ -0,0 +1,16 @@
+
+
+
+int main(int argc, char const *argv[]) {
+
+  // Problem 3
+  int N = 6;    // size of matrix
+  arma::mat A = arma::mat(N, N).fill(0.);
+
+  for (int i = 0; i < N; i++){
+    
+  }
+  double A_ij = A(i,j); //Assign element (i,j) of the matrix A to A_ij.
+
+  return 0;
+}
