a696541a3d0ce02db0e44a074bc70699ddffb11a
fridtjrg <fridtjof@gjengset.com>
2021-09-24 12:05:29 +0200

    Update with comments

--- project2/plot6.py
+++ project2/plot6.py
@@ -5,33 +5,32 @@ import sys
 
 
 
-#defines x, u and v lists
+
 N =[]
 itterations =[]
 
 
-#----------------Task2: plotting u------------
+#reads from datasets and inserts into lists
 with open("task6_dataset.txt") as myfile: #opens file
-    myfile.readline()
-    for line in myfile:                 #reads each line of the file
-        mydata = line.split(",")       #splits each line with the seperator
-        N.append(float(mydata[0]))      #Appends values to x list
-        itterations.append(float(mydata[1]))      #Appends values to u list
+    myfile.readline()   #skips first line
+    for line in myfile:              
+        mydata = line.split(",")      
+        N.append(float(mydata[0]))  
+        itterations.append(float(mydata[1]))      
 
-myfile.close()  #closes file
+myfile.close()  
 
-
+#Converts to array
 N = np.array(N)
 itterations = np.array(itterations)
 
 
-#plots function u(x) 
-
+#plots function 
 plt.plot(N,itterations, label ="Number of rotations")   
 
 
 
-
+#Sets up, saves and shows figure
 plt.xlabel("N values")
 plt.ylabel("Number of rotations")
 plt.title("Number of rotations when A is a symmetrical tridiagonal NxN matrix")
