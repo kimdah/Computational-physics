c4e734c7a04e172b162d878d1699e5ad73c138e1
cecilmkl <cklarpaas@gmail.com>
2021-10-01 11:50:59 +0200

    PenningTrap in progress

--- project3/penningtrap.cpp
+++ project3/penningtrap.cpp
@@ -1,36 +1,63 @@
-#include "PenningTrap.hpp";
+#include "PenningTrap.hpp"
+#include "Particle.hpp"
 
+
+//q = particle_[0].q_;
 
 
 // Constructor
-PenningTrap::PenningTrap(double B0_in, double V0_in, double d_in);
+PenningTrap::PenningTrap(double B0_in, double V0_in, double d_in){
+  B0_ = B0_in; // definer disse
+  V0_ = V0_in;
+  d_ = d_in;
+}
 
 // Add a particle to the trap
-void PenningTrap::add_particle(Particle p_in);
+void PenningTrap::add_particle(Particle p_in){
+  particles_.push_back(p_in);
+}
 
 // External electric field at point r=(x,y,z)
-arma::vec PenningTrap::external_E_field(arma::vec r);
+arma::vec PenningTrap::external_E_field(arma::vec r){
+  return arma::vec(1);
+
+}
 
 // External magnetic field at point r=(x,y,z)
-arma::vec PenningTrap::external_B_field(arma::vec r);
+arma::vec PenningTrap::external_B_field(arma::vec r){
+  return arma::vec(1);
+
+}
 
 // Force on particle_i from particle_j
-arma::vec PenningTrap::force_particle(int i, int j);
+arma::vec PenningTrap::force_particle(int i, int j){
+  return arma::vec(1);
+
+}
 
 // The total force on particle_i from the external fields
-arma::vec PenningTrap::total_force_external(int i);
+arma::vec PenningTrap::total_force_external(int i){
+  return arma::vec(1);
+
+}
 
 // The total force on particle_i from the other particles
-arma::vec PenningTrap::total_force_particles(int i);
+arma::vec PenningTrap::total_force_particles(int i){
+  return arma::vec(1);
+
+}
 
 // The total force on particle_i from both external fields and other particles
-arma::vec PenningTrap::total_force(int i);
+arma::vec PenningTrap::total_force(int i){
+  return arma::vec(1);
+}
 
 // Evolve the system one time step (dt) using Runge-Kutta 4th order
-void PenningTrap::evolve_RK4(double dt);
+void PenningTrap::evolve_RK4(double dt){
+
+}
 
 // Evolve the system one time step (dt) using Forward Euler
-void PenningTrap::evolve_forward_Euler(double dt);
+void PenningTrap::evolve_forward_Euler(double dt){
 
-
-q = particle_[0].q_;
+}
--- project3/penningtrap.hpp
+++ project3/penningtrap.hpp
@@ -4,42 +4,46 @@
 #include "Particle.hpp"
 
 #include <vector>
+#include <armadillo>
 
+class PenningTrap
+{
 
-class PenningTrap(){
+  public:
+    std::vector<Particle> particles_;  // container of particles
 
-public:
-  std::vector<Particle> particles_;
+    double B0_, V0_, d_;
 
+    // Constructor
+    PenningTrap(double B0_in, double V0_in, double d_in);
 
-  // Constructor
-  PenningTrap(double B0_in, double V0_in, double d_in);
+    // Add a particle to the trap
+    void add_particle(Particle p_in);
 
-  // Add a particle to the trap
-  void add_particle(Particle p_in);
+    // External electric field at point r=(x,y,z)
+    arma::vec external_E_field(arma::vec r);
 
-  // External electric field at point r=(x,y,z)
-  arma::vec external_E_field(arma::vec r);
+    // External magnetic field at point r=(x,y,z)
+    arma::vec external_B_field(arma::vec r);
 
-  // External magnetic field at point r=(x,y,z)
-  arma::vec external_B_field(arma::vec r);
+    // Force on particle_i from particle_j
+    arma::vec force_particle(int i, int j);
 
-  // Force on particle_i from particle_j
-  arma::vec force_particle(int i, int j);
+    // The total force on particle_i from the external fields
+    arma::vec total_force_external(int i);
 
-  // The total force on particle_i from the external fields
-  arma::vec total_force_external(int i);
+    // The total force on particle_i from the other particles
+    arma::vec total_force_particles(int i);
 
-  // The total force on particle_i from the other particles
-  arma::vec total_force_particles(int i);
+    // The total force on particle_i from both external fields and other particles
+    arma::vec total_force(int i);
 
-  // The total force on particle_i from both external fields and other particles
-  arma::vec total_force(int i);
+    // Evolve the system one time step (dt) using Runge-Kutta 4th order
+    void evolve_RK4(double dt);
 
-  // Evolve the system one time step (dt) using Runge-Kutta 4th order
-  void evolve_RK4(double dt);
+    // Evolve the system one time step (dt) using Forward Euler
+    void evolve_forward_Euler(double dt);
 
-  // Evolve the system one time step (dt) using Forward Euler
-  void evolve_forward_Euler(double dt);
+};
 
-}
+#endif
