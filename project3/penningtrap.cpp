df7c2a45d41aadc913bec1158a0208b57e4ea24b
cecilmkl <cklarpaas@gmail.com>
2021-10-01 10:40:50 +0200

    PenningTrap structre

--- project3/penningtrap.cpp
+++ project3/penningtrap.cpp
@@ -1,30 +1,33 @@
+#include "PenningTrap.hpp";
+
+
 
 // Constructor
-PenningTrap(double B0_in, double V0_in, double d_in);
+PenningTrap::PenningTrap(double B0_in, double V0_in, double d_in);
 
 // Add a particle to the trap
-void add_particle(Particle p_in);
+void PenningTrap::add_particle(Particle p_in);
 
 // External electric field at point r=(x,y,z)
-arma::vec external_E_field(arma::vec r);
+arma::vec PenningTrap::external_E_field(arma::vec r);
 
 // External magnetic field at point r=(x,y,z)
-arma::vec external_B_field(arma::vec r);
+arma::vec PenningTrap::external_B_field(arma::vec r);
 
 // Force on particle_i from particle_j
-arma::vec force_particle(int i, int j);
+arma::vec PenningTrap::force_particle(int i, int j);
 
 // The total force on particle_i from the external fields
-arma::vec total_force_external(int i);
+arma::vec PenningTrap::total_force_external(int i);
 
 // The total force on particle_i from the other particles
-arma::vec total_force_particles(int i);
+arma::vec PenningTrap::total_force_particles(int i);
 
 // The total force on particle_i from both external fields and other particles
-arma::vec total_force(int i);
+arma::vec PenningTrap::total_force(int i);
 
 // Evolve the system one time step (dt) using Runge-Kutta 4th order
-void evolve_RK4(double dt);
+void PenningTrap::evolve_RK4(double dt);
 
 // Evolve the system one time step (dt) using Forward Euler
-void evolve_forward_Euler(double dt);
+void PenningTrap::evolve_forward_Euler(double dt);
