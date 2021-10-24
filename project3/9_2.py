283fab766203584d2f4fd1b577ffb426bb3ed00c
fridtjrg <fridtjof@gjengset.com>
2021-10-24 16:46:22 +0200

    plotting dims

--- project3/9_2.py
+++ project3/9_2.py
@@ -22,7 +22,20 @@ x12 = np.array(data1[:,2])
 y12 = np.array(data1[:,3])
 
 
-fig, axes = plt.subplots(1,2, sharey=True)#,figsize=(7, 7)) # default:6.4, 4.8 sharex = True, sharey=True
+SMALL_SIZE = 17
+MEDIUM_SIZE = 17
+BIGGER_SIZE = 17
+
+plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
+plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+
+
+fig, axes = plt.subplots(1,2, sharey=True,figsize=(8,4))#,figsize=(7, 7)) # default:6.4, 4.8 sharex = True, sharey=True
 
 # Plotting no interaction vs interaction:
 
