f9d57f56167f3fb56fb68a6a02fbd08b53882b5c
cecilmkl <cklarpaas@gmail.com>
2021-10-25 19:35:11 +0200

    temp 10.7

--- project3/10_7.py
+++ project3/10_7.py
@@ -1,2 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+data_0 = np.loadtxt('./Results/problem10_f0.1broad.txt')
+data_1 = np.loadtxt('./Results/problem10_f0.1narrow with interactions.txt')
