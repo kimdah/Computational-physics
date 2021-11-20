ad505c1e6c98cf852d43239793115fa40c7dbcc6
cecilmkl <cklarpaas@gmail.com>
2021-11-20 11:40:45 +0100

    Analytical method

--- project4/main.cpp
+++ project4/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <string>
 #include <assert.h>
 #include <vector>
 #include <cmath>
@@ -11,7 +12,8 @@
 using namespace std;
 // Performs simulations based on parameter inputs
 double simulator(int n_cycles, int lattice_side_length, double T, int seed, int ordered_spin, string filen);
-void project4();
+void problem4();
+void analytical_2x2(double T);
 
 int main(int argc, char const *argv[]) {
   int T, L, n_cycles, ordered_spin, seed;
@@ -25,7 +27,7 @@ int main(int argc, char const *argv[]) {
       std::cerr << executable_name << " <Temperature (integer)>"
       << " <lattice side size (integer)>" << " <MCMC cycles (integer)>"
       << " <unordered lattice: use 0, ordered lattice: use -1 or 1>"<< std::endl;
-      project4();
+      problem4();
       return 0;
     } else if (argc == 6) {
       T = atoi(argv[1]);
@@ -69,44 +71,42 @@ double simulator(int n_cycles, int lattice_side_length, double T, int seed, int 
   return 42.42;
 }
 
-void project4() {
-  // Do all the things we need for Project 4 here
+void problem4() {
+  // Do all the things we need for Problem 4 here
   int cycles = 10000;
-  simulator(cycles/100, 20, 2.4, 1337, 0, "task4.txt");
+  double temp = 1.0;
+  simulator(cycles/100, 20, temp, 1337, 0, "task4.txt");
+  analytical_2x2(temp);
 }
 
-//-------- TRIED TO GET THE ANALYTICAL VALUES AND ESTIMATES FOR 2X2 TO FILE - NOT WORKING
+void analytical_2x2(double T){  // Maybe in Ising.cpp?
+  //double kB = 1.38064852 * pow(10, -23); // Remove?
+  double beta = 1. /T;
 
-// void test_with_2x2(n_cycles){
-//   ofstream tfile;
-//   tfile.open("values_table_2x2.txt");
-//   int width = 16;
-//   int prec  = 8;
-//   tfile << setw(width) << setprecision(prec) << scientific << "Sample#";
-//   tfile << setw(width) << setprecision(prec) << scientific << "E";
-//   tfile << setw(width) << setprecision(prec) << scientific << "M";
-//   tfile << endl;
-//   analytical_2x2(tfile);
-//
-//   Ising ising(2, 1.0, 2134, 0);
-//
-//   for (int i = 0; i < n_cycles; i++) {
-//     ising.analytical_2x2(tfile);
-//     // for (int j = 0; j < n_cycles/100; j++) {
-//     //   ising.run_metropolis_MCMC();
-//     // }
-//   }
-//   tfile.close();
-// }
-//
-// void analytical_2x2(){  // Maybe in Ising.cpp?
-//   vector<double> analytical_values;
-//   double kB = 1.38064852 * pow(10, -23); // Remove?
-//   double beta = 1. / (kB*T_);
-//
-//   double Z = 2*exp(beta*8) + 2*exp(-beta*8) + 12;
-//   double exp_val_epsilon = (4./Z) * (exp(-beta*8) - exp(beta*8));
-//   double exp_val_abs_mag = (2./Z) * (exp(beta*8) + 2);
-//   double heat_capacity = (32./(kB*pow(T,2)*Z))*(exp(-beta*8)-exp(beta*8)- (2./Z)*(exp(-beta*16)+exp(beta*16)-2));
-//   double susceptibility = (2/(kB*T*Z)) * (exp(8*beta) +1 - ((2./Z)*(exp(16*beta)+ 4*exp(8*beta)+4)));
-// }
+  double Z = 2*exp(beta*8) + 2*exp(-beta*8) + 12;
+  double exp_val_epsilon = (4./Z) * (exp(-beta*8) - exp(beta*8));
+  double exp_val_abs_mag = (2./Z) * (exp(beta*8) + 2);
+  double heat_capacity = (32./(pow(T,2)*Z))*(exp(-beta*8)-exp(beta*8)- (2./Z)*(exp(-beta*16)+exp(beta*16)-2));
+  double susceptibility = (2/(T*Z)) * (exp(8*beta) +1 - ((2./Z)*(exp(16*beta)+ 4*exp(8*beta)+4)));
+
+  // Write to file
+  ofstream ofile;
+  ofile.open("./datafiles/analytical_2x2_T=" +to_string(T) +".txt");
+  int width = 16;
+  int prec  = 5;
+
+  ofile << setw(width) << setprecision(prec) << scientific << "T";
+  ofile << setw(width) << setprecision(prec) << scientific << "<eps>";
+  ofile << setw(width) << setprecision(prec) << scientific << "<m>";
+  ofile << setw(width) << setprecision(prec) << scientific << "C_v";
+  ofile << setw(width) << setprecision(prec) << scientific << "Chi";
+  ofile << endl;
+
+  ofile << setw(width) << setprecision(prec) << scientific << T;
+  ofile << setw(width) << setprecision(prec) << scientific << exp_val_epsilon;
+  ofile << setw(width) << setprecision(prec) << scientific << exp_val_abs_mag;
+  ofile << setw(width) << setprecision(prec) << scientific << heat_capacity;
+  ofile << setw(width) << setprecision(prec) << scientific << susceptibility;
+  ofile << endl;
+  ofile.close();
+}
