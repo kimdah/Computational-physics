e1defab3519f83f1e73a5b1809f570223a2801ae
fridtjrg <fridtjof@gjengset.com>
2021-11-20 17:56:11 +0100

    Updating plotting

--- project4/plot_files/problem4.py
+++ project4/plot_files/problem4.py
@@ -23,31 +23,35 @@ plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
 plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
 
 
-data_values =  ['samples','E','M', 'expval_eps', 'expval_m', 'C_V', 'sucept']
+data_units =  ['Samples','E','M', r'[<\eps>]=J', r'[<m>] = 1', r'$[C_v] = $', r"[$\chi$] = $\frac{1}{k_B}$"]
+data_labels = ['Samples','E','M', 'eps', 'm', 'C_v', "susceptibility "]
 data = np.loadtxt('../datafiles/task4.txt',skiprows=1)
 analytical_values = np.loadtxt('../datafiles/analytical_2x2_T=1.0.txt',skiprows=1)
 
 
-fig, ax = plt.subplots(figsize = (6, 5))
-plt.subplots_adjust(
-top=0.95,
-bottom=0.15,
-left=0.15,
-right=0.98,
-hspace=0.2,
-wspace=0.2
-)
+
+
 
 for i in range(3,7):
-
+	fig, ax = plt.subplots(figsize = (6, 5))
+	plt.subplots_adjust(
+	top=0.95,
+	bottom=0.15,
+	left=0.2,
+	right=0.95,
+	hspace=0.2,
+	wspace=0.2
+	)
 	plt.plot(np.array(data[:,0]),np.array(data[:,i]), label="MCMC")
 	plt.plot(np.array(data[:,0]),np.ones(len(data[:,0]))*analytical_values[i-2], label='Analytical')
 	#ax.set_title("Training mse")
-	ax.set_ylabel(data_values[i])
-	ax.set_xlabel(data_values[0])
+	ax.set_ylabel(data_units[i])
+	ax.set_xlabel(data_units[0])
 	plt.ticklabel_format(axis="x", style="sci", scilimits=(0,0))
+	plt.ticklabel_format(axis="y", style="sci", scilimits=(0,0))
 	plt.grid()
-	plot_name = "Task4_"+data_values[0]+"_"+data_values[i]+"_figure.pdf"
+	plot_name = "Task4_"+data_labels[0]+"_"+data_labels[i]+"_figure.pdf"
 	plt.legend()
 	plt.savefig('../figures/'+plot_name)
 	plt.clf()
+	plt.cla()
