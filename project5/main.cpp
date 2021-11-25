a16d8e0bdc843c44cd149a319a04f62b66ce32c2
fridtjrg <fridtjof@gjengset.com>
2021-11-25 14:46:59 +0100

    not many changes

--- project5/main.cpp
+++ project5/main.cpp
@@ -16,16 +16,15 @@ using namespace std;
 using namespace arma;
 // Performs simulations based on parameter inputs
 int change_index(int i, int j, int M);
+void make_matrix(int n, float r,vector<float> a);
 
 
 
 int main(int argc, char const *argv[]) {
 
+//cout << change_index(3,3,5)<<endl;
 
 }
 
-//changes index for the u vector(column)
-//i and j can have values from 0 to M-2
-int change_index(int i, int j, int M){
-	return ((i%(M-1))-1)+ (M-2)*(j-1);
-}
+//changes index for the u vector(column), i and j can have values from 0 to M-2
+int change_index(int i, int j, int M){return ((i%(M-1))-1)+ (M-2)*(j-1);}
