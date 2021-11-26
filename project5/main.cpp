6ab6411b229d4948ccb74ebf41f592a8c4548c39
cecilmkl <cklarpaas@gmail.com>
2021-11-26 17:51:15 +0100

    matrix making with submatrices done, initializing special case(2.3) in progress

--- project5/main.cpp
+++ project5/main.cpp
@@ -13,31 +13,35 @@ using namespace std;
 using namespace arma;
 // Performs simulations based on parameter inputs
 int change_index(int i, int j, int M);
-void make_matrix(int n, float r,vector<float> a);
+void make_matrices(double r, cx_vec a, cx_vec b);
+sp_cx_mat make_matrix(double r, cx_vec d);
 rowvec time_step(rowvec u);
 
 
 
 int main(int argc, char const *argv[]) {
-  int M=5;
-  double deltat;
-  double h;
-  sp_cx_mat A= sp_cx_mat((m-2)^2, (m-2)^2).fill:zeros;
-  sp_cx_mat B= sp_cx_mat((m-2)^2, (m-2)^2).fill:zeros;
-  sp_cx_mat V= sp_cx_mat((m-2)^2, (m-2)^2).fill:zeros;
-  cx_vec a = cx_vec((m-2)^2-1);
-  cx_vec b = cx_vec((m-2)^2-1);
-
-  for(int k=0 ; k < (m-2)^2 ; k++){
-    a(k) = (1 + 4*r, deltat/2 * V(k,k);
-    b(k) = (1 - 4*r, -deltat/2 * V(k,k);
-    A(k,k) = a(k);
-    B(k,k) = b(k);
-  }
+  // int M=5;
+  // double deltat;
+  // double h;
+  // sp_cx_mat A= sp_cx_mat((m-2)^2, (m-2)^2).fill:zeros;
+  // sp_cx_mat B= sp_cx_mat((m-2)^2, (m-2)^2).fill:zeros;
+  // sp_cx_mat V= sp_cx_mat((m-2)^2, (m-2)^2).fill:zeros;
+  // cx_vec a = cx_vec((m-2)^2-1);
+  // cx_vec b = cx_vec((m-2)^2-1);
+  //
+  // for(int k=0 ; k < (m-2)^2 ; k++){
+  //   a(k) = (1 + 4*r, deltat/2 * V(k,k);
+  //   b(k) = (1 - 4*r, -deltat/2 * V(k,k);
+  //   A(k,k) = a(k);
+  //   B(k,k) = b(k);
+  // }
 
 //cout << change_index(3,3,5)<<endl;
 
-
+  // // Testing make_matrix methods (task 2.2)
+  // cx_vec aa(9, fill::ones); // fill::randu
+  cx_vec t(2*2, fill::value(3));
+  make_matrix_gen(2, t);
 
 }
 
@@ -46,5 +50,53 @@ int change_index(int i, int j, int M){return ((i%(M-1))-1)+ (M-2)*(j-1);}
 
 rowvec time_step(rowvec u){
 	rowvec b = affmul(B,u); //Calculates Bu = b (maybe cross() instead?)
-	return spsolve(A,b);	//spsolve assumes spars matix, maybe solve() instead.
+	return spsolve(A,b);	//spsolve assumes sparse matix, maybe solve() instead.
+}
+
+
+// Makes specialized A and B matrices (2.3)
+void make_matrices(int M, int h, double deltat, sp_cx_mat V, double r){
+  int mat_size = pow(M-1,2);
+  cx_vec a = cx_vec(mat_size);
+  cx_vec b = cx_vec(mat_size);
+
+  for(int k = 0 ; k < mat_size ; k++){
+    a(k) = (1 + 4*r, (deltat/2) * V(k,k));
+    b(k) = (1 - 4*r, -deltat/2 * V(k,k));
+  }
+
+  sp_cx_mat A = make_matrix(-r, a);
+  sp_cx_mat B = make_matrix(r,b);
+  cout << "A:\n" << A << "\nB:\n" << B << endl;
+  // perhaps make it return the matrices somehow?
+
+}
+
+// Makes a matrix on the general form of A and B
+sp_cx_mat make_matrix(double r, cx_vec d){
+  int S = d.size(); // (M-2)^2
+  int s = sqrt(S); // (M-2)
+  sp_cx_mat M = sp_cx_mat(S, S);
+
+  // Making diag
+  for (int i = 0; i < S; i+=s){ // last index i = S-s
+    sp_cx_mat D(s,s);
+    D.diag() = d.subvec(i,i+s-1);//cx_vec(s,fill::value(d(i))); // diagonal
+    D.diag(-1) = cx_vec(s-1,fill::value(r)); // subdiagonal
+    D.diag(1) = cx_vec(s-1,fill::value(r)); // superdiagonal
+    M.submat(i,i,i+s-1,i+s-1) = D; //submat(first_row, first_col, last_row, last_col)
+  }
+
+  // Making non-diag, non-corners
+  for (int i = s; i < S; i+=s){ // last index i = S-s
+    sp_cx_mat ND(s,s);
+    ND.diag() = cx_vec(s,fill::value(8)); // fill diagonal with r value
+    //submat(first_row, first_col, last_row, last_col)
+    M.submat(i-s,i,i-1,i+s-1) = ND; //feks: i=s=3: (0,3,2,5) i=2s=6: (3,6,5,8)
+    M.submat(i, i-s, i+s-1, i-1) = ND; // feks i=s=3: (3,0,5,2) i=2s=6: (6,3,8,5)
+  }
+
+  // Corners are 0 matrix, which they are already initialized as through sp_ (sparse)
+
+  return M;
 }
