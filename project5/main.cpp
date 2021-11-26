22d1b0e2c8a711d8e6fbed0a692c45e974a01436
cecilmkl <cklarpaas@gmail.com>
2021-11-26 18:44:29 +0100

    2.3 in progress part 2

--- project5/main.cpp
+++ project5/main.cpp
@@ -6,16 +6,19 @@
 #include <cmath>
 #include <iomanip>
 #include <chrono>
+#include <complex>
 #include <armadillo>
 
 
 using namespace std;
 using namespace arma;
+using namespace std::complex_literals;
 // Performs simulations based on parameter inputs
 int change_index(int i, int j, int M);
-void make_matrices(double r, cx_vec a, cx_vec b);
+//void make_matrices(double r, cx_vec a, cx_vec b);
+void make_matrices(int M, double h, double deltat, sp_cx_mat V, double r);
 sp_cx_mat make_matrix(double r, cx_vec d);
-rowvec time_step(rowvec u);
+//rowvec time_step(rowvec u);
 
 
 
@@ -41,28 +44,40 @@ int main(int argc, char const *argv[]) {
   // // Testing make_matrix methods (task 2.2)
   // cx_vec aa(9, fill::ones); // fill::randu
   cx_vec t(2*2, fill::value(3));
-  make_matrix_gen(2, t);
+  //make_matrix(2, t);
+  sp_cx_mat V = sp_cx_mat(9,9);
+  V.diag() = cx_vec(9, fill::ones);
+
+
+  make_matrices(5, 0.1, 0.1, V, 2);
 
 }
 
 //changes index for the u vector(column), i and j can have values from 0 to M-2
 int change_index(int i, int j, int M){return ((i%(M-1))-1)+ (M-2)*(j-1);}
 
-rowvec time_step(rowvec u){
-	rowvec b = affmul(B,u); //Calculates Bu = b (maybe cross() instead?)
-	return spsolve(A,b);	//spsolve assumes sparse matix, maybe solve() instead.
-}
+// rowvec time_step(rowvec u){
+// 	rowvec b = affmul(B,u); //Calculates Bu = b (maybe cross() instead?)
+// 	return spsolve(A,b);	//spsolve assumes sparse matix, maybe solve() instead.
+// }
 
 
 // Makes specialized A and B matrices (2.3)
-void make_matrices(int M, int h, double deltat, sp_cx_mat V, double r){
-  int mat_size = pow(M-1,2);
+void make_matrices(int M, double h, double deltat, sp_cx_mat V, double r){
+  int mat_size = pow(M-2,2);
   cx_vec a = cx_vec(mat_size);
   cx_vec b = cx_vec(mat_size);
 
   for(int k = 0 ; k < mat_size ; k++){
-    a(k) = (1 + 4*r, (deltat/2) * V(k,k));
-    b(k) = (1 - 4*r, -deltat/2 * V(k,k));
+    cout << "V real: " << V(k,k).real();
+    double real = (deltat/2) * V(k,k).real();
+    cx_double img = (deltat/2) * V(k,k).imag();
+    cout << "real: "<< real << "\nimag: "<< img << endl;
+    //cx_double img = (deltat/2) * V(k,k);
+    //cx_double img = (deltat/2) * V(k,k);
+    //cout << "V: "<< V(k,k) << "\nimg: " << endl;
+    //a(k) = (1 + 4*r + 1i*((deltat/2)*V(k,k))); // (real, imaginary)
+    //b(k) = (1 - 4*r + 1i*((-deltat/2)*V(k,k)));
   }
 
   sp_cx_mat A = make_matrix(-r, a);
