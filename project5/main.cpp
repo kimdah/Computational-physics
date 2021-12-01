6f24bd4e9bb2cf8691a537947ceef8691a61e3c5
Kim Dahl-Hansen <kimda@student.matnat.uio.no>
2021-12-01 02:00:05 +0100

    Lots of mess here

--- project5/main.cpp
+++ project5/main.cpp
@@ -18,7 +18,7 @@ using namespace arma;
 
 
 // Performs simulations based on parameter inputs
-int change_index(int i, int j, int M);
+int get_k_index(int i, int j, int M);
 void make_matrices(int M, double h, double deltat, sp_cx_mat V, double r);
 sp_cx_mat make_matrix(double r, cx_vec d);
 cx_vec time_step(sp_cx_mat A, sp_cx_mat B, cx_vec u);
@@ -36,7 +36,7 @@ int main(int argc, char const *argv[]) {
 
   // Some of code demands Armadillo version 10.6 - check your version here:
   // arma::arma_version ver;
-	// std::cout << "ARMA version: "<< ver.as_string() << std::endl;
+  // std::cout << "ARMA version: "<< ver.as_string() << std::endl;
 
   // Testing make_matrices
   sp_cx_mat V = sp_cx_mat(9,9);
@@ -49,13 +49,20 @@ int main(int argc, char const *argv[]) {
   //makes matrces A and B
   make_matrices(5, 0.1, 0.1, V, 2);
   sp_cx_mat U = make_wavepacket(5, 0.1, 0.1, 0.1, 0.2, 0.2, 0.1, 0.1);
-  cout<< U <<endl;
+  
+  for (int i = 0; i<5; i++) {
+    for (int j = 0; j<5; j++) {
+      cout << setw(25) << U(i,j);
+    }
+    cout << endl;
+  }
+  //cout << U << endl;
 
 
 
 }
 
-//crates matix U at time t=0
+//crates matrix U at time t=0
 sp_cx_mat make_wavepacket(int M, double h, double x_c, double y_c, double sigma_x, double sigma_y, double p_x, double p_y){
 
   sp_cx_mat U = sp_cx_mat(M, M); //Creates the matrix U
@@ -69,9 +76,9 @@ sp_cx_mat make_wavepacket(int M, double h, double x_c, double y_c, double sigma_
     }
   }
 
-  cx_double bc= (0,1); //boundary condition(Need to find correcct) only works with imaginary != 0
+  cx_double bc= (0,1); //boundary condition(Need to find correct) only works with imaginary != 0
 
-  cout << M <<endl;
+  //cout << M <<endl;
   //Filling in boundary conditions
   for(int i=0; i < M+1; i+=(M-1)){ //Should change loop conditions to make more clear
     for(int j=0; j< M; j++){
@@ -106,23 +113,31 @@ sp_cx_mat make_wavepacket(int M, double h, double x_c, double y_c, double sigma_
   return U;
 }
 
-//changes index for the u vector(column), i and j can have values from 1 to M-2 
-int change_index(int i, int j, int M){return ((i%(M-1))-1)+ (M-2)*(j-1);}
+// Problem 2-1
+// Translates matrix (i,j) index to column (k) index which have values from 1 to M-2 
+int get_k_index(int i, int j, int M){
+//return ((i % (M - 1)) - 1) + (M - 2) * (j - 1);
+  return ((j - 1) * (M - 2)) + (i - 1);
+  
+  }
 
 // Task3
- cx_vec time_step(sp_cx_mat A, sp_cx_mat B, cx_vec u){
- 	int m_size = B.size();
- 	//cx_vec b = affmul(B,u.t()); //Calculates Bu = b (maybe cross() instead?) (did not work)
- 	cx_vec b = cx_vec(m_size);
+cx_vec time_step(sp_cx_mat A, sp_cx_mat B, cx_vec u){
+  int m_size = B.size();
+  // Try to get this to work for part 1
+  //cx_vec b = affmul(B,u.t()); //Calculates Bu = b (maybe cross() instead?) (did not work). 
+  cx_vec b = cx_vec(m_size);
 
- 	//matrix multiplication Bu=b(instead of affmul()
- 	for(int i =0;i< m_size; i++){
- 		for(int j =0;j< m_size; j++){
- 			b(i) += (u(i).real()*B(i,j).real()-B(i,j).imag()*u(i).imag())+1i*(u(i).real()*B(i,j).imag()+u(i).imag()*B(i,j).real());
- 		}
- 	}
- 	return spsolve(A,b.t());	//spsolve assumes sparse matix. Solves the matrix eq. Ax=b, maybe solve() instead.
- 	}
+  // Try to optimise this
+  //matrix multiplication Bu=b(instead of affmul()
+  for(int i =0;i< m_size; i++){
+    for(int j =0;j< m_size; j++){
+      b(i) += (u(i).real()*B(i,j).real()-B(i,j).imag()*u(i).imag())+1i*(u(i).real()*B(i,j).imag()+u(i).imag()*B(i,j).real());
+    }
+  }
+  // PArt 2
+  return spsolve(A,b.t());	//spsolve assumes sparse matix. Solves the matrix eq. Ax=b, maybe solve() instead.
+ }
 
 
 // Makes specialized A and B matrices (Task 2.3)
@@ -135,8 +150,8 @@ void make_matrices(int M, double h, double deltat, sp_cx_mat V, double r){
   for(int k = 0 ; k < mat_size ; k++){
     double real = (deltat/2) * V(k,k).real(); // these work, but gives unnecessary work
     double img = (deltat/2) * V(k,k).imag();
-    a(k) = cx_double(1 + 4*r - img, real); // assuming r is real
-    b(k) = cx_double(1 - 4*r + img, -real);
+    // a(k) = cx_double(1 + 4*r - img, real); // assuming r is real
+    // b(k) = cx_double(1 - 4*r + img, -real);
    
     // We want these to work(behold til gruppetime paa torsdag):
     cout << "i*V: " << cx_double(V(k,k)) * 1.0i << endl; 
@@ -145,7 +160,7 @@ void make_matrices(int M, double h, double deltat, sp_cx_mat V, double r){
   }
 
   A = make_matrix(-r, a); // made these global variables - maybe make a class instead?
-  B = make_matrix(r,b);
+  B = make_matrix(r,  b);
 
 }
 
@@ -157,12 +172,12 @@ sp_cx_mat make_matrix(double r, cx_vec d){
 
   // Making diag
   for (int i = 0; i < S; i+=s){ // last index = S-s
-    sp_cx_mat D(s,s);
-    D.diag() = d.subvec(i,i+s-1);// diagonal
+    sp_cx_mat D(s, s);
+    D.diag() = d.subvec(i, i+s-1);// diagonal
     D.diag(-1) = cx_vec(s-1).fill(r); // subdiagonal
     D.diag(1) = cx_vec(s-1).fill(r); // superdiagonal
     //submat(first_row, first_col, last_row, last_col)
-    M.submat(i,i,i+s-1,i+s-1) = D; //ex: (0,0,2,2), (3,3,5,5)
+    M.submat(i, i, i+s-1, i+s-1) = D; //ex: (0,0,2,2), (3,3,5,5)
   }
 
   // Making non-diag, non-corners
@@ -170,7 +185,7 @@ sp_cx_mat make_matrix(double r, cx_vec d){
     sp_cx_mat ND(s,s);
     ND.diag() = cx_vec(s).fill(r); // fill diagonal with r value
     //submat(first_row, first_col, last_row, last_col)
-    M.submat(i-s,i,i-1,i+s-1) = ND; //ex: i=s=3: (0,3,2,5) i=2s=6: (3,6,5,8)
+    M.submat(i-s, i, i-1, i+s-1) = ND; //ex: i=s=3: (0,3,2,5) i=2s=6: (3,6,5,8)
     M.submat(i, i-s, i+s-1, i-1) = ND; //ex: i=s=3: (3,0,5,2) i=2s=6: (6,3,8,5)
   }
 
