7e0456ea2ae08555a4e743b17ac1569db8672e33
fridtjrg <fridtjof@gjengset.com>
2021-12-04 16:42:12 +0100

    adding plotfile

---
+++ project5/plotfiles/plotting.py
@@ -0,0 +1,5 @@
+import pyarma as pa
+
+A = pa.mat() #Create pa.mat object (just as arma::mat in C++)
+A.load("../datafiles/test.dat") #Load the content of the matrix you saved into your Python program.
+
